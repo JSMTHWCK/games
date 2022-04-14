b00f3e0c3c784d7be47fee109d52134ba5253e0b
JSMTHWCK <jeffrey.smithwick@gmail.com>
2022-04-14 08:57:53 -0700

    so many pyc's fixed run game to work elias style, updated ben py

--- tic-tac-toe/run_game.py
+++ tic-tac-toe/run_game.py
@@ -1,21 +1,22 @@
 from tic_tac_toe import *
 import custom_strat
 from random_strat import random_move
-import ben
+from ben import ben
 from elias import elias
 from manual import manual_move
 from smthwck import jeff
 from player import Player
-score = {'Player 1' : 0, 'Player 2' : 0, 'Tie' : 0}
-score2 = {'Player 1' : 0, 'Player 2':0, 'Tie':0}
+outcomes = {'Jeffrey' : 0, 'ben' : 0, 'Tie' : 0}
 past = {}
 strat = 1
-for i in range(0,50000):
+for i in range(0,2):
     if i%1000 == 0:
         print(i)
-    a = Game(Player(jeff),Player(elias))
-    b = Game(Player(elias),Player(jeff))
-    score[a.game()] += 1
-    score2[b.game()] += 1
-print('where player 1 is jeffrey ', score)
-print('where player 1 is random ', score2)
+    if i%2 == 0:
+        a = Game(Player(jeff),Player(ben))
+        player_order = {'Tie': 'Tie', 'Player 1': 'Jeffrey', 'Player 2': 'ben'}
+    else:
+        a = Game(Player(ben),Player(jeff))
+        player_order = {'Tie': 'Tie', 'Player 1': "ben", 'Player 2': 'Jeffrey'}
+    outcomes[player_order[a.game(log = True)]] += 1
+print(outcomes)
--- tic-tac-toe/smthwck.py
+++ tic-tac-toe/smthwck.py
@@ -1,22 +1,13 @@
 from helpers import *
-past = {}
-strat = 1
+
 def jeff(board):
     corners = [0,2,6,8]
     edges = [1,3,5,7]
     middle = [4]
     p_turn = 1 if len(possible_moves(board))%2 == 1 else 2
-    if len(past) >= 5:
-        lost_game = [k for k,v in past.items() if v == 'Loss']
-        if len(lost_game) * 5 < len(past):
-            print('strat switched')
-            strat = 2
-
     if p_turn == 1:
-        if len(possible_moves(board)) == 9 and strat == 1:
+        if len(possible_moves(board)) == 9:
             return 8 #takes corner
-        elif len(possible_moves(board)) == 9 and strat == 2:
-            return 4
     if p_turn == 2 and len(possible_moves(board)) == 8:
         if board[4] == 0:
             return 4
@@ -24,29 +15,16 @@ def jeff(board):
             return 8
     '''-------------- set = 1 ended ----------------'''
     if p_turn == 1 and len(possible_moves(board)) == 9 - 2:
-        if strat == 1:
-            i = board.index(p_turn+1)
-            if i in middle:
-                return 0
-            elif i in corners:
-                if i + board.index(1) == 8:
-                    return 2
-                else:
-                    return 8 - i
-            elif i in edges:
-                return 4
-        if strat == 2:
-            a = find_all(2,board)[0] 
-            if a < 3:
-                if i in edges:
-                    return 0
-                else:
-                   return 8 - i 
-            elif a < 6:
-                if a == 5:
-                    return 8
-                elif a == 3:
-                    return 6
+        i = board.index(p_turn+1)
+        if i in middle:
+            return 0
+        elif i in corners:
+            if i + board.index(1) == 8:
+                return 2
+            else:
+                return 8 - i
+        elif i in edges:
+            return 4
             
     if p_turn == 2 and len(possible_moves(board)) == 9 - 3:
         if len(is_almost_end(board)[0]) == 0:
